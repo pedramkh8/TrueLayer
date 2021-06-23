7b7ccb2aa5716a8ff3b32f2462a7ffd9688e09d5
Pedram <alipourpedram@gmail.com>
2021-06-23 22:59:15 +0100

    Add logging.

---
+++ Pokedex.Api/Middleware/ErrorLoggingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Pokedex.Api.Middleware
+{
+	public class ErrorLoggingMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger<ErrorLoggingMiddleware> logger;
+
+		public ErrorLoggingMiddleware(RequestDelegate next, ILogger<ErrorLoggingMiddleware> logger)
+		{
+			_next = next;
+			this.logger = logger;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (Exception e)
+			{
+				logger.LogError(e, e.Message);
+				throw;
+			}
+		}
+	}
+}
--- Pokedex.Api/Program.cs
+++ Pokedex.Api/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Pokedex.Api
 {
@@ -13,7 +10,22 @@ namespace Pokedex.Api
 		{
 				public static void Main(string[] args)
 				{
-						CreateHostBuilder(args).Build().Run();
+						var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+						try
+						{
+								logger.Debug("init main function");
+								CreateHostBuilder(args).Build().Run();
+						}
+						catch (Exception ex)
+						{
+								logger.Error(ex, "Error in init");
+								throw;
+						}
+						finally
+						{
+								NLog.LogManager.Shutdown();
+						}
 				}
 
 				public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +33,12 @@ namespace Pokedex.Api
 								.ConfigureWebHostDefaults(webBuilder =>
 								{
 										webBuilder.UseStartup<Startup>();
-								});
+								})
+								.ConfigureLogging(logging =>
+								{
+										logging.ClearProviders();
+										logging.SetMinimumLevel(LogLevel.Information);
+								})
+										.UseNLog();
 		}
 }
